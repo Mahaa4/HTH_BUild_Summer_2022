71fd601c005bdf0fc91e37de636225bed1df99de
mahaa4 <you@example.com>
2022-06-30 11:38:28 -0700

    adding getting started with python file

---
+++ Getting-Started-with-Python-mahaa4pranav/about_me.py
@@ -0,0 +1,22 @@
+full_name = "Mahaalakshumi Narendar"
+nickname = "Mahaa"
+age = 17
+python_experience = True
+
+hobbies = [" Listening to music", "baking", "watching anime and Kdramas", "sleeping", "eating", "staring at a wall"]
+
+favorites = {
+  "movie": "Top Gun Maverick",
+  "number": 4,
+  "food": "Ramen",
+  "hobby": hobbies[0],
+  "anime": "Ouran High School Host CLub",
+}
+
+print(full_name, nickname, age, python_experience)
+print()
+print(hobbies)
+print()
+print(favorites)
+
+all_vars = dict(vars())
